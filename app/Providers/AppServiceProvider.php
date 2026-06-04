eff13a9084d55efcb967eeabc4ac295bb281c137
VanisaFaradela <faradelavanisa@gmail.com>
2026-06-04 21:00:38 +0700

    memperbaiki AppService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('layouts.app', function ($view) {
-            $tipeRumah = TipeRumah::with('cluster')->limit(4)->get();
+            $tipeRumah = TipeRumah::limit(4)->get();
 
             $view->with('tipeRumah', $tipeRumah);
         });
