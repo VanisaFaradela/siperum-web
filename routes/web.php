e4142b99bd3650487cc09c5eec697b561eab092a
VanisaFaradela <faradelavanisa@gmail.com>
2026-06-05 13:15:45 +0700

    memperbaiki tipe rumah error

--- routes/web.php
+++ routes/web.php
@@ -88,13 +88,15 @@ Route::get('/media/cluster/{file}', function ($file) {
 Route::get('/tipe-detail/{id}', [TipeRumahController::class, 'detail'])->name('tipe-detail');
 Route::get('/media/tipe-rumah/{file}', function ($file) {
 
-    $path = '/home/u143856011/shared/uploads/tipe-rumah/' . $file;
+    $path = public_path('uploads/tipe-rumah/' . $file);
 
-    abort_unless(file_exists($path), 404);
+    if (!file_exists($path)) {
+        abort(404);
+    }
 
     return response()->file($path);
 
-})->where('file', '.*')->name('media.tipe-rumah');
+})->name('media.tipe-rumah');;
 
 // Route untuk Tentang Kami
 Route::get('/tentang', [PageController::class, 'tentang'])->name('tentang');
